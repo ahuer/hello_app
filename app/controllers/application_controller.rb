8bef6b18c5695765cd133977739f86a02772d8b1
Ayron Huer <ayron.huer@gmail.com>
2016-12-20 14:15:33 -0500

    testing updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "¡Hola, mundo!"
+    render html: "Kickin ass on Heroku"
   end
 
   def goodbye
